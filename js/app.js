b8a27f0fb44be74614c1c408b507ce3e06a03789
abdukarim <kaumijabdukarim@gmail.com>
2025-02-20 17:10:31 +0500

    123

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 // alert('hello');
+document.addEventListener("DOMContentLoaded", () => {
+    const burger = document.getElementById("burger");
+    const mobileMenu = document.getElementById("mobileMenu");
+
+    burger.addEventListener("click", () => {
+        mobileMenu.classList.toggle("active");
+        burger.classList.toggle("active");
+    });
+});
