c3258b773abb192864890676ede0b61afde5324f
abdukarim <kaumijabdukarim@gmail.com>
2025-02-22 14:22:16 +0500

    123

--- js/app.js
+++ js/app.js
@@ -8,3 +8,19 @@ document.addEventListener("DOMContentLoaded", () => {
         burger.classList.toggle("active");
     });
 });
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 17,
+    slidesPerView: 7,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+  var swiper2 = new Swiper(".mySwiper2", {
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+});
